0e67198131acdbcd889639819f42b7e3071d7f5f
xizlt <37120642+xizlt@users.noreply.github.com>
2019-10-13 20:48:09 +0300

    регулярка

--- index.php
+++ index.php
@@ -3,49 +3,40 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
 require_once 'vendor/autoload.php ';
-
 header('Content-type: text/html; charset=utf-8');
-
-$url = 'https://mobile-review.com/review/samsung-galaxy-s10-plus.shtml';
-//$url = 'http://forum.php.su/topic.php?forum=71&topic=16207';
-$file = file_get_contents($url);
-$pattern = '#<iframe[^<]+src="[^<]+www.youtube.com/embed/(\w+)[^<]+</iframe>#is';
-preg_match_all($pattern, $file, $matches, PREG_PATTERN_ORDER);
 
 $api_key = 'AIzaSyDSNud-QsEcx1vZlQyqEhgLzwq6jg1mMCQ';
 
-    for ($i = 0; $i < 3; $i++) {
-        $video_url = $matches[1][$i];
-        $api_url = 'https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=' . $video_url . '&key=' . $api_key;
-        $data = json_decode(file_get_contents($api_url));
+$url = 'https://mobile-review.com/review/samsung-galaxy-s10-plus.shtml';
 
-        echo '<strong>Title: </strong>' . $data->items[0]->snippet->title . '<br>';
-        echo '<strong>publishedAt: </strong>' . $data->items[0]->snippet->publishedAt . '<br>';
-        echo '<strong>Duration: </strong>' . $data->items[0]->statistics->viewCount . '<br>';
-}
+$file = file_get_contents($url);
 
-class YouTubeVideo
-{
-    public $id; //id видео
-    private $apiKey = 'AIzaSyDSNud-QsEcx1vZlQyqEhgLzwq6jg1mMCQ';
-    private $youtube;
+$pattern = '~(?:http|https|)(?::\/\/|)(?:www.|)(?:youtu\.be\/|youtube\.com(?:\/embed\/|\/v\/|\/watch\?v=|\/ytscreeningroom\?v=|\/feeds\/api\/videos\/|\/user\S*[^\w\-\s]|\S*[^\w\-\s]))([\w\-]{11})[a-z0-9;:@#?&%=+\/\$_.-]*~i';
+preg_match_all($pattern, $file, $matches);
 
-    public function __construct()
-    {
+$count_matches = count($matches[1]);
+if (!empty($count_matches)) {
+    for ($i = 0; $i < $count_matches; $i++) {
+        $ids = $matches[1][$i];
+
         $client = new Google_Client();
-        $client->setDeveloperKey($this->apiKey);
-        $this->youtube = new Google_Service_YouTube($client);
-    }
+        $client->setDeveloperKey($api_key);
+        $service = new Google_Service_YouTube($client);
 
+        $results = $service->videos->listVideos('snippet, statistics, contentDetails', ['id' => $ids,]);
 
-    /*
-    * Получение данных видео по их id
-    */
-    public function videosByIds(string $ids)
-    {
-        return $this->youtube->videos->listVideos('snippet, statistics, contentDetails', [
-            'id' => $ids,
-        ]);
+        $path = 'result.txt';
+        foreach ($results as $item)
+            if (!empty($item)) {
+                $you = [
+                    'Описание: ' . $item['snippet']['description'] . "\n",
+                    'Название видео: ' . $item['snippet']['title'] . "\n",
+                    'Дата публикации: ' . $item['snippet']['publishedAt'] . "\n",
+                    'Просмотров: ' . $item['statistics']['viewCount'] . "\n\n",
+                ];
+                file_put_contents($path, $you, FILE_APPEND);
+            }
     }
 }
